ff89a3b0575cd1545f809b9b25e35435b361ae30
Nick <nickrray@gmail.com>
2020-07-09 10:41:00 -0500

    :sparkles: Implement users array, pushing to array and checking for a user existing inside it

--- client/scripts.js
+++ client/scripts.js
@@ -1,5 +1,31 @@
-document.querySelector('form').addEventListener('submit', (event) => {
+const regForm = document.querySelector('.regForm')
+const loginForm = document.querySelector('.login-form')
+
+const users = []
+
+regForm.addEventListener('submit', (event) => {
   event.preventDefault()
 
-  console.log('Form has been submitted!')
+  const newUser = {
+    email: regForm.querySelector('#email').value,
+    phone: regForm.querySelector('#phone').value,
+    name: regForm.querySelector('#name').value,
+    username: regForm.querySelector('#username').value,
+    password: regForm.querySelector('#password').value
+  }
+
+  console.log(newUser) // TESTING
+  users.push(newUser)
+})
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  // Get username and password values
+  const username = loginForm.querySelector('#user-login-input').value
+  const pw = loginForm.querySelector('#password-login-input').value
+
+  // Use findIndex to check if users[] contains that pairing
+  const userIndex = users.findIndex(user => user.username === username && user.password === pw)
+  console.log(userIndex)
+  console.log('A user logged in!!!')
 })
